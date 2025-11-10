c082b52979068768f3505fe5d78ffc1e513b526d
bharathidev0303 <bharathiraja996574@gmail.com>
2025-11-10 09:32:00 +0530

    orders

--- android/app/.cxx/RelWithDebInfo/504n6b2r/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/saravanakumar/.gradle/caches/8.14.3/transforms/bbf7357ad895cc3671b199768384f733/transformed/hermes-android-0.81.1-release/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/saravanakumar/.gradle/caches/8.14.3/transforms/bbf7357ad895cc3671b199768384f733/transformed/hermes-android-0.81.1-release/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
