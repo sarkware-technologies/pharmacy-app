b04c517dc7b9d8b7c15458c6f0d53d1d02f052c4
jayrajvmu <jayarajvmu@gmail.com>
2025-12-23 21:22:31 +0530

    sun pharma endpoint

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -7,8 +7,8 @@ import { store } from "../redux/store";
 import { logout } from "../redux/slices/authSlice";
 
 
-export const BASE_URL = 'https://pharmsupply-dev-api.pharmconnect.com';
-// export const BASE_URL = 'https://dev-specialrates-api.sunpharma.cloud';
+// export const BASE_URL = 'https://pharmsupply-dev-api.pharmconnect.com';
+export const BASE_URL = 'https://dev-specialrates-api.sunpharma.cloud';
 
 class ApiClient {
     constructor() {
